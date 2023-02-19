56ab30af355b63bfb72495e88bb6a031a1804d25
IvanKahinga <ikahinga@gmail.com>
2023-02-19 14:57:08 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr.Chocolate', 5, 'M', 'brown', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Rowdy', 8, 'M', 'black', 'Aggressive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Tabitha', 3, 'F', 'grey', 'Calm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Sergeant Brown', 5, 'M', 'brown', 'Calm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Melissa', 17, 'F', 'brown', 'Aggrssive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Grinch', 20, 'M', 'black', 'Timid', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Wendy', 12, 'F', 'black', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+(NULL, NULL, 'M', 'brown', 'Aggressive', 1);
